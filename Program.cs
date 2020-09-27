f1bd0af09bb7adefdbec88117313b73e8b60fbc8
Aseem <aseemanand2@gmail.com>
2020-09-27 17:55:19 +0530

    [Aseem] Added UC2-Daily Employee Wage

--- Program.cs
+++ Program.cs
@@ -9,21 +9,29 @@ namespace EmpWageComputation
             Console.WriteLine("Welcome to Employee Wage Computation");
 
 
-            //UC1-Employee Attendance Check
-
             int if_full_time = 1;
+            int empWagePerHour = 20;
+            int fullDayHour = 0;
+            int empWage = 0;
+
+
             Random randNum = new Random();
-            int empCheck = randNum.Next(1, 2);
+            int empCheck = randNum.Next(0, 2);
             if(empCheck == if_full_time)
             {
                 Console.WriteLine("Employee is present");
+                fullDayHour = 8;
             }
             else
             {
                 Console.WriteLine("Employee is absent");
+                fullDayHour = 0;
             }
 
+            // Employee Wage
 
+            empWage = empWagePerHour * fullDayHour;
+            Console.WriteLine("Employee Wage for the day: "+empWage);
         }
     }
 }
