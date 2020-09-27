29904000c9e4ee8840d91fd07c38f3f492bf2dab
Aseem <aseemanand2@gmail.com>
2020-09-27 17:29:58 +0530

    [Aseem] Added UC1-Attendance Check

--- Program.cs
+++ Program.cs
@@ -7,6 +7,23 @@ namespace EmpWageComputation
         static void Main(string[] args)
         {
             Console.WriteLine("Welcome to Employee Wage Computation");
+
+
+            //UC1-Employee Attendance Check
+
+            int if_full_time = 1;
+            Random randNum = new Random();
+            int empCheck = randNum.Next(1, 2);
+            if(empCheck == if_full_time)
+            {
+                Console.WriteLine("Employee is present");
+            }
+            else
+            {
+                Console.WriteLine("Employee is absent");
+            }
+
+
         }
     }
 }
