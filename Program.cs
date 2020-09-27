bd8b1764ec7b5b4fa3d24b06b1df031fba3c40e1
Aseem <aseemanand2@gmail.com>
2020-09-27 19:25:54 +0530

    [Aseem] Conditional Final Wage Calculation

--- Program.cs
+++ Program.cs
@@ -16,26 +16,29 @@ namespace EmpWageComputation
             int monthlyWage = 0;
             int workHour = 0;
             int empWage = 0;
+            int working_days = 1;
+            int total_work_hour = 0;
+            int final_workdays = 0;
 
 
-            for(int day=1; day <= no_of_working_days;day++)
+            while(total_work_hour<=100 && working_days <=20 )
             {
                 Random randNum = new Random();
                 int empCheck = randNum.Next(0, 3);
                 switch (empCheck)
                 {
                     case if_full_time:
-                        Console.WriteLine("Day {0}: Employee is present",day);
+                        Console.WriteLine("Day {0}: Employee is present",working_days);
                         workHour = 8;
                         break;
 
                     case if_part_time:
-                        Console.WriteLine("Day {0}: Employee is present and working part time",day);
+                        Console.WriteLine("Day {0}: Employee is present and working part time", working_days);
                         workHour = 4;
                         break;
 
                     default:
-                        Console.WriteLine("Day{0}: Employee is absent",day);
+                        Console.WriteLine("Day {0}: Employee is absent", working_days);
                         workHour = 0;
                         break;
 
@@ -43,8 +46,11 @@ namespace EmpWageComputation
                 empWage = empWagePerHour * workHour;
                 monthlyWage += empWage;
                 Console.WriteLine("Employee Wage for the day: " + empWage);
+                final_workdays = working_days;
+                working_days++;
+                total_work_hour += workHour;
             }
-            Console.WriteLine("\n" + "Monthly wage is: "+monthlyWage);
+            Console.WriteLine("\n" + "Monthly wage is: "+monthlyWage+"\n"+"Hours worked : "+ total_work_hour + "\n"+"Days worked: "+ final_workdays);
            
         }
     }
